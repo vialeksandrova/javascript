e4626d5b77854fc89f3628751f6b9701b713739b
vi aleksandrova <viialeksandrova@gmail.com>
2014-03-25 22:43:54 +0200

    Create years.php

---
+++ years.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+<script type="text/javascript">
+var year=prompt("Input year","1987");
+old=2020-year;
+alert("In 2020 your years are "+old);
+</script>
+</body>
+</html>
