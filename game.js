e2e1dbe958cd0c4c91b2e83437f8267d44410427
vi aleksandrova <viialeksandrova@gmail.com>
2014-03-31 22:37:06 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+/*Популярната игра "камък, ножица, хартия"*/
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 if(userChoice!=="rock" && userChoice!=="paper" && userChoice!=="scissors"){
     console.log("Wrong choice");
