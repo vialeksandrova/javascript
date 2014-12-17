1283d903e073b86f8fb442789684ed677bf028a1
vi aleksandrova <vialeksandrova@users.noreply.github.com>
2014-12-17 14:50:51 +0200

    Create cashregister.js

---
+++ cashregister.js
@@ -0,0 +1,43 @@
+//отстъпка за персонал
+function StaffMember(name,discountPercent){
+    this.name = name;
+    this.discountPercent = discountPercent;
+}
+
+var iva = new StaffMember("Iva",5);
+var bobby = new StaffMember("Bobby",10);
+var me = new StaffMember("Vi",20);
+
+var cashRegister = {
+    total:0,
+    lastTransactionAmount: 0,
+    add: function(itemCost){
+        this.total += (itemCost || 0);
+        this.lastTransactionAmount = itemCost;
+    },
+    scan: function(item,quantity){
+        switch (item){
+        case "eggs": this.add(0.98 * quantity); break;
+        case "milk": this.add(1.23 * quantity); break;
+        case "magazine": this.add(4.99 * quantity); break;
+        case "chocolate": this.add(0.45 * quantity); break;
+        }
+        return true;
+    },
+    voidLastTransaction : function(){
+        this.total -= this.lastTransactionAmount;
+        this.lastTransactionAmount = 0;
+    },
+    applyStaffDiscount: function(employee){
+        this.total -= this.total*(employee.discountPercent/100);
+    }
+    
+};
+
+cashRegister.scan('eggs',1);
+cashRegister.scan('milk',1);
+cashRegister.scan('magazine',3);
+cashRegister.applyStaffDiscount(me);
+
+// крайният резултат
+console.log('Your bill is '+cashRegister.total.toFixed(3));
